a3d05883741d7549ec89345a7b79d16b12c7f726
seanboss-bit <shaunaibek@gmail.com>
2025-10-28 10:28:46 +0100

    white logo iphone fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         name="apple-mobile-web-app-status-bar-style"
         content="white-translucent"
       />
-      <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+      <link rel="apple-touch-icon" href="/images/logo-white.png" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       <meta name="apple-mobile-web-app-title" content="Paraplug" />
